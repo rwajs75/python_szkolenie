1334ac9e4dd970ded4b943a81823993ecefd0dfb
Roman Wajs <rwajs@isolution.pl>
2017-10-31 09:52:07 +0100

    dodanie nowej osoby do książki

---
+++ data_adress.py
@@ -0,0 +1,17 @@
+__autor__ = 'Roman'
+
+
+class Data_adress:
+    def __init__(self, firstname, lastname, nickname, company, adress, home, mobile, work, fax, email, byear):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.adress = adress
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.byear = byear
+
