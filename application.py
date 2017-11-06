2766101632b1a8c1427e2ff601bf5d672ecf363d
Roman Wajs <rwajs@isolution.pl>
2017-11-06 12:36:55 +0100

    log błędu

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(3)
+        self.wd.implicitly_wait(5)
 
     def open_home_page(self):
         wd = self.wd
