4b91fdc623b1b6f98902d837fdc9a814cb2a28b8
Roman Wajs <rwajs@isolution.pl>
2017-11-06 21:20:11 +0100

    wyodrębnienie klas do kontaktów

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contacts import ContactsHelper
 
 class Application:
 
@@ -9,6 +10,7 @@ class Application:
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contacts = ContactsHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -16,70 +18,3 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
-
-    def details(self):
-        wd = self.wd
-        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[7]/a/img").click()
-
-    def create_new_adress_book(self, data_adress):
-        wd = self.wd
-        self.open_add_new()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(data_adress.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys()
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(data_adress.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(data_adress.nickname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(data_adress.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(data_adress.adress)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(data_adress.home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(data_adress.mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(data_adress.work)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(data_adress.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(data_adress.email)
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[1]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[1]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[4]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[4]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[4]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[4]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[25]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[25]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[25]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[25]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[1]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[1]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[1]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[1]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[1]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[1]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[3]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[3]").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(data_adress.byear)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def open_add_new(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
--- test/test_add_adress_book.py
+++ test/test_add_adress_book.py
@@ -12,20 +12,18 @@ def app(request):
     return fixture
 
 def test_add_new(app):
-    app.login(password="secret", username="admin")
-    app.create_new_adress_book(Contacts(firstname="Roman", lastname="Wajs", nickname="rwajs",
-                                        company="własna działalność", adress="ul. Czapli 3, 05-830 Nadarzyn", home="159753123",
-                                        mobile="123654789", work="987654123", fax="456852159", email="rwajs@tlen.pl", byear="1975"))
-    app.details()
-    app.logout()
+    app.session.login(password="secret", username="admin")
+    app.contacts.create(Contacts(firstname="Roman", lastname="Wajs", nickname="rwajs",
+                                 company="własna działalność", adress="ul. Czapli 3, 05-830 Nadarzyn", home="159753123",
+                                 mobile="123654789", work="987654123", fax="456852159", email="rwajs@tlen.pl", byear="1975"))
+    app.session.logout()
 
 def test_add_new1(app):
-    app.login(password="secret", username="admin")
-    app.create_new_adress_book(Contacts(firstname="Roman1", lastname="Wajs1", nickname="rwajs1",
-                                        company="własna działalność1", adress="ul. Czapli 3, 05-830 Nadarzyn1", home="159753123",
-                                        mobile="123654789", work="987654123", fax="456852159", email="rwajs1@tlen.pl", byear="1979"))
-    app.details()
-    app.logout()
+    app.session.login(password="secret", username="admin")
+    app.contacts.create(Contacts(firstname="Roman1", lastname="Wajs1", nickname="rwajs1",
+                                 company="własna działalność1", adress="ul. Czapli 3, 05-830 Nadarzyn1", home="159753123",
+                                 mobile="123654789", work="987654123", fax="456852159", email="rwajs1@tlen.pl", byear="1979"))
+    app.session.logout()
 
 
 
