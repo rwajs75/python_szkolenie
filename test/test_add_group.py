967824f884308da790c23d6b444652c0d75bfa63
Roman Wajs <rwajs@isolution.pl>
2017-11-06 20:29:55 +0100

    wyodrębniona klasa do kierowania sesjami

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,11 +12,11 @@ def app(request):
     return fixture
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="test2", header="test2", footer="test2"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empy_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
