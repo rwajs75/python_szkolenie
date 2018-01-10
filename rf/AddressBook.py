7d1f99c649ca98a1aa7c85f954d387bd1f285d44
Roman Wajs <rwajs@isolution.pl>
2018-01-10 21:14:11 +0100

    RF2

--- rf/AddressBook.py
+++ rf/AddressBook.py
@@ -27,5 +27,14 @@ class AddressBook:
     def destroy_fixtures(self):
         self.fixture.destroy()
 
-    def create_group(self, name, header, footer):
-        self.fixture.group.create(Group(name=name, header=header, footer=footer))
+    def new_group(self, name, header, footer):
+        return Group(name=name, header=header, footer=footer)
+
+    def get_group_list(self):
+        return self.dbfixture.get_group_list()
+
+    def create_group(self, group):
+        self.fixture.group.create(group)
+
+    def group_list_should_be_equal(self, list1, list2):
+        assert sorted(list1, key=Group.id_or_max) == sorted(list2, key=Group.id_or_max)
--- rf/group_scenarios.robot
+++ rf/group_scenarios.robot
@@ -1,9 +1,15 @@
 *** Settings ***
 Library  rf.AddressBook
+Library  Collections
 Suite Setup  Init Fixtures
 Suite Teardown  Destroy Fixtures
 
 
 *** Test Cases ***
 Add new group
-    Create Group  name10  header10  footer10
+    ${old_list}=  Get Group List
+    ${group}=  New Group  name10  header10  footer10
+    Create Group  ${group}
+    ${new_list}=  Get Group List
+    Append To List  ${old_list}  ${group}
+    Group List Should Be Equal  ${new_list}  ${old_list}
